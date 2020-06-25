f0dd1f9bfecd442828568e470dcc06126dbdeba5
lennard <lennardbaeumer@gmail.com>
2020-06-25 09:11:59 +0200

    Train working

--- pytorch-pneumonia/train.py
+++ pytorch-pneumonia/train.py
@@ -4,7 +4,7 @@ import loadData
 import torch.nn.functional as F
 from Net import Net
 from settings import epochs, device
-from torch import optim
+from torch import optim, nn
 from torch.autograd import Variable
 from utils import log
 
@@ -22,37 +22,36 @@ def train(e):
         type = Variable(type).to(device)
         optimizer.zero_grad()
         out = model(data)
-        criterion = F.nll_loss
+        criterion = nn.NLLLoss()
         loss = criterion(out, type)
         loss.backward()
         optimizer.step()
-        # log(f"Epoch {e}/{epochs}\t{batch_id * len(data)}/{len(trainloader)}\tLoss: {loss.data[0]}")
-        log(f"{batch_id}/{len(trainloader)} finished")
-        log(f"Loss: {loss.data}")
+
+
+        # log(f"{batch_id}/{len(trainloader)} finished")
+        # log(f"Loss: {loss.data}")
         batch_id += 1
     log(f"Epoch {e}/{epochs} finished")
 
 
 def test():
     model.eval()
-    correct=0
-    len_test=len(testloader)
+    correct = 0
+    len_test = len(testloader)
     files = os.listdir("Data/test/")
     for data, type in testloader:
-        type=Variable(type).to(device)
+        type = Variable(type).to(device)
         data = Variable(data).to(device)
-        out=model(data)
+        out = model(data)
 
         if type == out.data.max(1, keepdim=True)[1]:
-            correct+=1
-            print(correct)
-    print(correct/len_test*100+"%")
-        #print(out.data.max(1, keepdim=True)[1])
+            correct += 1
 
+    print(str(correct / len_test * 100) + "%")
 
 
 if __name__ == '__main__':
-    #train(1)
-    test()
+
     for e in range(1, epochs + 1):
-        pass
+        train(e)
+        test()
